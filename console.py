7f427600fe494f977304b89af37e138a8baa81fa
JeremyWarui <jerywarne7@gmail.com>
2023-02-13 01:05:57 +0300

    Update console func

--- console.py
+++ console.py
@@ -115,55 +115,34 @@ class HBNBCommand(cmd.Cmd):
         Usage:
         update <class name> <id> <attribute name> "<attribute value>"
         """
-        args = tuple(entry.split())
-        if len(args) >= 4:
-            key = "{}.{}".format(args[0], args[1])
-            cast = type(eval(args[3]))
-            arg3 = args[3]
-            arg3 = arg3.strip('"')
-            arg3 = arg3.strip("'")
-            setattr(storage.all()[key], args[2], cast(arg3))
-            storage.all()[key].save()
-        elif len(args) == 0:
+        if len(entry) == 0:
             print("** class name missing **")
-        elif args[0] not in HBNBCommand.classes:
+            return
+
+        args = entry.split(" ")
+        cls_name = args[0]
+        objs = storage.all()
+
+        if cls_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
         elif len(args) == 1:
             print("** instance id missing **")
-        elif ("{}.{}".format(args[0], args[1])) not in storage.all().keys():
+        elif ("{}.{}".format(args[0], args[1])) not in objs.keys():
             print("** no instance found **")
         elif len(args) == 2:
             print("** attribute name missing **")
-        else:
+        elif len(args) == 3:
             print("** value missing **")
-        # if len(entry) == 0:
-        #     print("** class name missing **")
-        #     return
+        elif len(args) >= 4:
+            obj_id = args[1]
+            name = "{}.{}".format(cls_name, obj_id)
+            attr = args[2]
+            value = args[3].strip("'").strip('"')
 
-        # args = entry.split(" ")
-        # cls_name = args[0]
-        # objs = storage.all()
-
-        # if cls_name not in HBNBCommand.classes:
-        #     print("** class doesn't exist **")
-        # elif len(args) == 1:
-        #     print("** instance id missing **")
-        # elif ("{}.{}".format(args[0], args[1])) not in objs.keys():
-        #     print("** no instance found **")
-        # elif len(args) == 2:
-        #     print("** attribute name missing **")
-        # elif len(args) == 3:
-        #     print("** value missing **")
-        # elif len(args) >= 4:
-        #     obj_id = args[1]
-        #     name = "{}.{}".format(cls_name, obj_id)
-        #     attr = args[2]
-        #     value = args[3].strip("'").strip('"')
-
-        #     for key, val in objs.items():
-        #         if key == name:
-        #             setattr(val, attr, value)
-        #             storage.save()
+            for key, val in objs.items():
+                if key == name:
+                    setattr(val, attr, value)
+                    storage.save()
 
     def do_count(self, cls_name):
         """ Displays number of instances of entered class """
